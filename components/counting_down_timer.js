6f94425cbcd9703a3b400a5b305d1b1b27608085
Cloud Hu <jingqihu@hotmail.com>
2020-01-03 11:12:50 -0600

    adding style

--- components/counting_down_timer.js
+++ components/counting_down_timer.js
@@ -10,7 +10,7 @@ class CounterDownTimer extends React.Component {
     };
 
     componentDidMount() {
-        const date = '2020-12-31';
+        const date = this.props.endDate;
 
         this.interval = setInterval(() => {
             var time = this.getTimeRemaining(date);
@@ -42,17 +42,90 @@ class CounterDownTimer extends React.Component {
 
     render() {
         var { days, hours, minutes, seconds } = this.state;
+
         return(
-            
-            <div id="clockdiv">
-                Days: <span className="days">{days}</span><br/>
-                Hours: <span className="hours">{hours}</span><br/>
-                Minutes: <span className="minutes">{minutes}</span><br/>
-                Seconds: <span className="seconds">{seconds}</span>
-            </div>          
+            <div>
+            <div className="container">
+                <h1 id="head">Countdown to 2020 Year:</h1>
+                <div className="clockContainer">
+                    <div id="clockdiv">
+                        <div>
+                            <span className="days">{days}</span>
+                            <div className="smalltext">Days</div>
+                        </div>
+                        <div>
+                            <span className="hours">{hours}</span>
+                            <div className="smalltext">Hours</div>
+                        </div>
+                        <div>
+                            <span className="minutes">{minutes}</span>
+                            <div className="smalltext">Minutes</div>
+                        </div>
+                        <div>
+                            <span className="seconds">{seconds}</span>
+                            <div className="smalltext">Seconds</div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <style jsx>
+                    {`
+                    .container > h1{
+                        text-align: center;
+                    }
+
+                    .clockContainer {
+                        display: flex;
+                        justify-content: center;
+                    }
+
+                    body{
+                        text-align: center;
+                        background: #00ECB9;
+                        font-family: sans-serif;
+                        font-weight: 100;
+                    }
+                    
+                    h1{
+                      color: #396;
+                      font-weight: 100;
+                      font-size: 40px;
+                      margin: 40px 0px 20px;
+                    }
+                    
+                    #clockdiv{
+                        font-family: sans-serif;
+                        color: #fff;
+                        display: inline-block;
+                        font-weight: 100;
+                        text-align: center;
+                        font-size: 30px;
+                    }
+                    
+                    #clockdiv > div{
+                        padding: 10px;
+                        border-radius: 3px;
+                        background: #00BF96;
+                        display: inline-block;
+                    }
+                    
+                    #clockdiv div > span{
+                        padding: 15px;
+                        border-radius: 3px;
+                        background: #00816A;
+                        display: inline-block;
+                    }
+                    
+                    .smalltext{
+                        padding-top: 5px;
+                        font-size: 16px;
+                    }
+                    
+                    `}
+                </style>
+            </div>        
         );
     }
-
 
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,16 @@ import CounterDownTimer from '../components/counting_down_timer'
 
 const Index = () => (
     <div>
-        <h1>Home Page</h1>
-        <CounterDownTimer/>
+        <h1>Demo</h1>
+        <CounterDownTimer endDate='2020-12-31'/>
+        <style jsx>
+            { `
+                h1 {
+                    text-align: center;
+                    font-size: 60px;
+                }
+            ` }
+        </style>
     </div>
 );
 
