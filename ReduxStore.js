8dde9baac2edd368c60e9547c264ceacb7f97c78
Cloud Hu <jingqihu@hotmail.com>
2020-01-14 13:50:17 -0600

    redux simplify

--- ReduxStore.js
+++ ReduxStore.js
@@ -1,44 +1,19 @@
 import React from 'react';
 import { initializeStore } from './store';
 
-const __NEXT_REDUX_STORE__ = '__NEXT_REDUX_STORE__';
-
-function getOrCreateStore(initialState) {
-    // if it is server side 
-    if (typeof window === 'undefined') {
-        return initializeStore(initialState);
-    }
-
-    // Create store if unavailable on the client and set it on the window object
-    if (!window[__NEXT_REDUX_STORE__]) {
-        window[__NEXT_REDUX_STORE__] = initializeStore(initialState);
-    }
-    return window[__NEXT_REDUX_STORE__];
-}
-
-
 export default App => class AppwithRedux extends React.Component {
 
-    static async getinitalProps(appContext) {
-        const reduxStore = getOrCreateStore();
-
-        // Provide the store to getInitialProps of pages
-        appContext.ctx.reduxStore = reduxStore;
-
-        let appProps = {};
-        if (typeof App.getInitialProps === 'function') {
-            appProps = await App.getInitialProps.call(App, appContext);
-        }
+    static async getInitalprops() {
+        const store = initializeStore();
 
         return {
-            ...appProps,
-            initialReduxState: reduxStore.getState(),
-        };
+            initialReduxState: store.getState(),
+        }
     }
 
     constructor(props) {
         super(props);
-        this.reduxStore = getOrCreateStore(props.initialReduxState);
+        this.reduxStore = initializeStore(props.initialReduxState);
     }
 
     render() {
